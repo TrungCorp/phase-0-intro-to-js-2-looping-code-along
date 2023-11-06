3f54bf20d06a4ca62237a029ace468ad74057988
TrungLe <trungle9995@gmail.com>
2023-11-06 09:19:52 -0500

    complete labs

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(obj,occasion)
+{
+    const copyArray =[]
+    for(let i=0;i<obj.length;i++)
+    {
+        copyArray.push(`Thank you, ${obj[i]}, for the wonderful ${occasion} gift!`)
+    }
+    return copyArray
+}
+function countDown(num)
+{
+    for(num;num>=0;num--)
+        console.log(num)
+}
